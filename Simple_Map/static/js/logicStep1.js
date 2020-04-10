02f48cd2c202f4a8eb6add892c8fe03e16174d46
Irene Kang <irenehk@Irenes-MacBook-Pro-2.local>
2020-04-10 13:19:52 -0700

    Uploading Earthquakes past 7 days data.

--- Simple_Map/static/js/logicStep1.js
+++ Simple_Map/static/js/logicStep1.js
@@ -18,38 +18,24 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 // Create a base layer that holds both maps.
 let baseMaps = {
     "Streets": streets,
-    "Satellite Streets": satelliteStreets
+    "Satellite": satelliteStreets
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [43.7, -79.3],
-	zoom: 11,
+	center: [39.5, -98.5],
+	zoom: 3,
 	layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/irenehk1213/Mapping_Earthquakes/master/torontoNeighborhoods.json";
-
-// Create a style for the lines.
-let myStyle = {
-	color: 'blue',
-    weight: 1, 
-    fillColor: 'yellow'
-}
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-      style: myStyle,
-      onEachFeature: function(feature, layer) {
-          layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>"); 
-      }
-  }).addTo(map);
+  L.geoJson(data).addTo(map);
 });
 
 
